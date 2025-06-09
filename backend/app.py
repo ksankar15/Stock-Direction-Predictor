71c45f88b54b8a5cfa64972a6fe8e6619af3aba7
Kavin Sankar <cary.sankars@gmail.com>
2025-06-09 17:14:00 -0400

    Fix port issue

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 from model import get_stock_prediction, get_chart
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -29,4 +30,5 @@ def chart():
         return jsonify({"error": str(e)}), 404
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
